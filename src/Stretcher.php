c48d75c220f7699ecff2abcd5ac324c02c512b24
Assouan Dinari <assouan.dinari@gmail.com>
2016-04-26 02:12:37 +0200

    Added Stretcher class

---
+++ src/Stretcher.php
@@ -0,0 +1,99 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class Stretcher
+{
+    /** @var callable */
+    protected $resolver;
+
+    /** @var \SplQueue */
+    protected $queue;
+
+    /**
+     * Stretcher constructor.
+     * @param callable|null $resolver
+     */
+    public function __construct(callable $resolver = null)
+    {
+        // Queue
+        $this->queue = new \SplQueue();
+        // Default resolver
+        $this->resolver = $resolver ? $resolver : function ($callable)
+        {
+            // If PHP callable
+            if (is_callable($callable))
+                return $callable;
+
+            // If custom callable
+            if (is_string($callable) AND
+                substr($callable, 0, 1) == '@' AND
+                $callable = substr($callable, 1) AND
+                $cc = explode(':', $callable))
+                // @Middleware
+                if (count($cc) == 1)
+                    return [new $cc[0], '__invoke'];
+                // @Controller:action
+                elseif (count($cc) == 2)
+                    return [new $cc[0], $cc[1]];
+
+            // Else throw exception
+            throw new Exception('Invalid callable argument(');
+        };
+    }
+
+    /**
+     * @return callable|null
+     */
+    public function getResolver()
+    {
+        return $this->resolver;
+    }
+
+    /**
+     * @param callable|null $resolver
+     * @return $this
+     */
+    public function setResolver(callable $resolver = null)
+    {
+        $this->resolver = $resolver;
+        return $this;
+    }
+
+    /**
+     * @param callable $callable
+     * @return callable
+     */
+    protected function resolve($callable) : callable
+    {
+        return ($resolver = $this->resolver) ? $resolver($callable) : $callable;
+    }
+
+    /**
+     * @param callable $callable
+     */
+    public function add($callable)
+    {
+        $this->queue->enqueue($callable);
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param callable[] ...$callables unshift callables (top priority)
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, ...$callables)
+    {
+        foreach ($callables as $callable)
+            $this->queue->unshift($callable);
+
+        if ($this->queue->isEmpty())
+            return $response;
+
+        $callable = $this->resolve($this->queue->dequeue());
+
+        return call_user_func($callable, $request, $response, $this);
+    }
+}
